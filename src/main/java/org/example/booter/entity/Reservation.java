59e649f04f1576f779a380ef138558156e94a742
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-05-18 12:53:32 +0200

    adding reservation entity

--- src/main/java/org/example/booter/entity/Reservation.java
+++ src/main/java/org/example/booter/entity/Reservation.java
@@ -30,4 +30,28 @@ public class Reservation {
                 ", resDate='" + resDate + '\'' +
                 '}';
     }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getGuestId() {
+        return guestId;
+    }
+
+    public void setGuestId(String guestId) {
+        this.guestId = guestId;
+    }
+
+    public Date getResDate() {
+        return resDate;
+    }
+
+    public void setResDate(Date resDate) {
+        this.resDate = resDate;
+    }
 }
