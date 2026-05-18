c7acb7879f02db177f408c55d86c44a604879f98
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-05-18 12:54:29 +0200

    adding reservation repository

--- src/main/java/org/example/booter/repository/ReservationRepository.java
+++ src/main/java/org/example/booter/repository/ReservationRepository.java
@@ -3,8 +3,9 @@ package org.example.booter.repository;
 import org.example.booter.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Date;
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository <Reservation, Integer> {
-    List<Reservation> getReservationsByResDate(String resDate);
+    List<Reservation> getReservationsByResDate(Date resDate);
 }
