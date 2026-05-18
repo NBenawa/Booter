7199f37d717268fe0df9c0cc9422b2569d6fbd25
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-05-18 12:52:14 +0200

    adding room entity and controller

--- src/main/java/org/example/booter/controller/RoomController.java
+++ src/main/java/org/example/booter/controller/RoomController.java
@@ -1,4 +1,24 @@
 package org.example.booter.controller;
 
+import org.example.booter.repository.RoomRespository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/rooms")
 public class RoomController {
+
+    private final RoomRespository roomRespository;
+
+    public RoomController(RoomRespository roomRespository) {
+        this.roomRespository = roomRespository;
+    }
+
+    @GetMapping
+    public String rooms(Model model) {
+        model.addAttribute("rooms", this.roomRespository.findAll());
+        return "rooms";
+    }
 }
--- src/main/java/org/example/booter/entity/Room.java
+++ src/main/java/org/example/booter/entity/Room.java
@@ -28,4 +28,28 @@ public class Room {
                 ", bedInfo='" + bedInfo + '\'' +
                 '}';
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getBedInfo() {
+        return bedInfo;
+    }
+
+    public void setBedInfo(String bedInfo) {
+        this.bedInfo = bedInfo;
+    }
 }
