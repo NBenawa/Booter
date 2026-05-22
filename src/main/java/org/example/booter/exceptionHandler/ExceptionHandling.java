95b5bf14fd3627758b030fdff8e4f185345f2b91
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-05-22 19:00:31 +0200

    completing exception handling class

--- src/main/java/org/example/booter/exceptionHandler/ExceptionHandling.java
+++ src/main/java/org/example/booter/exceptionHandler/ExceptionHandling.java
@@ -1,9 +1,7 @@
 package org.example.booter.exceptionHandler;
 
 import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,5 +12,15 @@ public class ExceptionHandling {
     @ExceptionHandler(GuestNotFoundException.class)
     public ResponseEntity<?> handleGuestNotFoundException(GuestNotFoundException exception) {
         return new ResponseEntity<>(new String[]{"Guest Not Found!", "A Guest with the given id is not in the database.", HttpStatusCode.valueOf(404).toString()}, HttpStatusCode.valueOf(404));
+    }
+
+    @ExceptionHandler(ReservationNotFoundException.class)
+    public ResponseEntity<?> handleReservationNotFoundException(ReservationNotFoundException exception) {
+        return new ResponseEntity<>(new String[]{"Reservation Not Found!", "A Reservation with this id is not in the database.", HttpStatusCode.valueOf(404).toString()}, HttpStatusCode.valueOf(404));
+    }
+
+    @ExceptionHandler(RoomNotFoundException.class)
+    public ResponseEntity<?> handleRoomNotFoundException(RoomNotFoundException exception) {
+        return new ResponseEntity<>(new String[]{"Room Not Found!", "A Room with given id is not found.", HttpStatusCode.valueOf(404).toString()}, HttpStatusCode.valueOf(404));
     }
 }
