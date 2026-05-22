97d361c14491c35ac74d445c85abf60a4ec4ad04
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-05-22 18:54:41 +0200

    adding error format

--- src/main/java/org/example/booter/exceptionHandler/ErrorFormat.java
+++ src/main/java/org/example/booter/exceptionHandler/ErrorFormat.java
@@ -1,4 +1,11 @@
 package org.example.booter.exceptionHandler;
 
 public class ErrorFormat {
+    final String heading;
+    final String details;
+
+    ErrorFormat(String heading, String details) {
+        this.heading = heading;
+        this.details = details;
+    }
 }
