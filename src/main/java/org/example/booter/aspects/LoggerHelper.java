0d63a393792c2204e6a5e47042dfc82e42b0400c
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-05-23 22:13:33 +0200

    writing logger helper

---
+++ src/main/java/org/example/booter/aspects/LoggerHelper.java
@@ -0,0 +1,21 @@
+package org.example.booter.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.example.booter.entity.Guest;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Component
+@Aspect
+public class LoggerHelper {
+
+    @After("execution(* org.example.booter.api.controller.GuestApiController.addGuest(..))")
+    public void writeLog(JoinPoint joinPoint) {
+        Guest guest = Guest.convertToGuest(joinPoint.getArgs()[0].toString());
+        LoggerWriter.writeLog(guest,"New Guest Created!", LocalDateTime.now());
+    }
+}
